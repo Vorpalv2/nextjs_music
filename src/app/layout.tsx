7a6bb58ed184c2dc49dd9690a9e543789ef261a9
Aakash <aakashanand1995@gmail.com>
2024-03-04 18:22:01 +0530

    hero section update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={inter.className}>
-        <div className="relative flex justify-center items-center w-full">
+        <div className="relative w-full flex items-center justify-center ">
           <Navbar />
         </div>
         {children}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,11 @@
 "use client";
+
 import React, { useState } from "react";
-import {
-  HoveredLink,
-  Menu,
-  MenuItem,
-  ProductItem,
-} from "@/components/ui/navbar-menu";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 
-// export function NavbarDemo() {
-//   return (
-//     <div className="relative w-full flex items-center justify-center">
-//       <Navbar className="top-2" />
-//       <p className="text-black dark:text-white">
-//         The Navbar will show on top of the page
-//       </p>
-//     </div>
-//   );
-// }
-
-export default function Navbar({ className }: { className?: string }) {
+function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
     <div
@@ -28,22 +13,31 @@ export default function Navbar({ className }: { className?: string }) {
     >
       <Menu setActive={setActive}>
         <Link href={"/"}>
-          <MenuItem setActive={setActive} active={active} item="Home" />
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
         </Link>
-
         <MenuItem setActive={setActive} active={active} item="Our Courses">
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/All-Courses">All Courses</HoveredLink>
-            <HoveredLink href="/Music-Theory">Basic Music Theory</HoveredLink>
-            <HoveredLink href="/Composition">Advance Composition</HoveredLink>
-            <HoveredLink href="/Songwriting">Songwriting</HoveredLink>
-            <HoveredLink href="/Production">Music Production</HoveredLink>
+            <HoveredLink href="/courses">All Courses</HoveredLink>
+            <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+            <HoveredLink href="/courses">Advanced Composition</HoveredLink>
+            <HoveredLink href="/courses">Songwriting</HoveredLink>
+            <HoveredLink href="/courses">Music Production</HoveredLink>
           </div>
         </MenuItem>
         <Link href={"/contact"}>
-          <MenuItem setActive={setActive} active={active} item="Contact Us" />
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Contact Us"
+          ></MenuItem>
         </Link>
       </Menu>
     </div>
   );
 }
+
+export default Navbar;

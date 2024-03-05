4cb0d5c3d5f405cda02cc4c97472a4d7f3f32c6e
Aakash <aakashanand1995@gmail.com>
2024-03-05 21:51:25 +0530

    added courses section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Testimonial from "@/components/Testimonial";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Instructors from "@/components/Instructors";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <Testimonial />
       <UpcomingWebinars />
       <Instructors />
+      <Footer />
     </main>
   );
 }

84fe728f1da9515fbd86b95e3aad20b836f9b337
Aakash <aakashanand1995@gmail.com>
2024-03-07 14:25:18 +0530

    contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import ContactUs from "@/components/ContactUs";
+import { Button } from "@/components/ui/moving-border";
 
-const Contact = () => {
-  return <div>Contact</div>;
+const page = () => {
+  return (
+    <div className="h-screen flex flex-col">
+      <div className="flex h-[40%] justify-center items-center">
+        <form action="/contact" method="post">
+          <input type="text" name="" id="" />
+          <input type="text" name="" id="" />
+          <Button type="submit">Submit</Button>
+        </form>
+        <ContactUs />
+      </div>
+    </div>
+  );
 };
 
-export default Contact;
+export default page;
---
+++ src/components/ContactUs.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BackgroundBeams } from "./ui/background-beams";
+
+const ContactUs = () => {
+  return (
+    <div>
+      <BackgroundBeams />
+    </div>
+  );
+};
+
+export default ContactUs;
